3e68c1e22ddfc9d33e4742297692f047d88c5159
Edward Delaporte <edthedev@gmail.com>
2014-08-24 22:12:13 -0500

    Back to working hello world.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,30 +1,15 @@
-//arduino bike speedometer w serial.print()
-//by Amanda Ghassaei 2012
-//http://www.instructables.com/id/Arduino-Bike-Speedometer/
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
-*/
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
 
-//pin connected to read switch
-#define read_pin A1
-#define hot_pin A2
-
-//storage variable
-int read_pin_value;
-
-void setup(){
-  Serial.begin(9600);
-  // Make hot pin hot.
-  digitalWrite(hot_pin, HIGH);
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
 }
 
-void loop(){
-  read_pin_value = analogRead(read_pin);
-  Serial.println(read_pin_value);
-  delay(10);
+void loop()
+{
+	// your main loop code here...
 }
